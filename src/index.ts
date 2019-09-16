39eaf7b516e99dc4175c84c747575b565bbd4dae
huyang2229 <254662830@qq.com>
2019-09-16 23:32:57 +0800

    feat(all the file): 增加headers url参数的处理

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { AxiosRequestConfig } from './types'
+import { buildURL } from './helpers/url'
+import xhr from './xhr'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
+  xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transfromRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transfromRequestData(config: AxiosRequestConfig): void {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+// 字符串字面量类型
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'patch'
+  | 'PATCH'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+}
