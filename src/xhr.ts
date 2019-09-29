37f70f94bb149ee569cacc5d1a15ddfcf5b39ba1
huyang2229 <254662830@qq.com>
2019-09-29 21:08:09 +0800

    feat(all): 获取相应数据,处理相应请求和相应data
    获取相应数据,处理相应请求和相应data

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,56 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
-export default function(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get', headers } = config
+export default function(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
 
-  const request = new XMLHttpRequest()
-  request.open(method.toUpperCase(), url, true)
+    const request = new XMLHttpRequest()
 
-  Object.keys(headers).forEach(name => {
-    if (!data && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    if (timeout) {
+      request.timeout = timeout
+    }
+
+    request.open(method.toUpperCase(), url, true)
+
+    Object.keys(headers).forEach(name => {
+      if (!data && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = request.responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+
+    request.ontimeout = function handletimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+    }
+
+    request.send(data)
+  })
 }
