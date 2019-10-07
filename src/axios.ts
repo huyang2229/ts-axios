676c337f45a79bb2bde61755636547fff730a744
huyang2229 <254662830@qq.com>
2019-10-07 12:27:43 +0800

    feat: 扩展axios.create静态方法创建新的axios实例

--- src/axios.ts
+++ src/axios.ts
@@ -1,16 +1,21 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosStatic, AxiosRequestConfig } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaultConfig from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaultConfig)
+
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaultConfig, config))
+}
 
 export default axios
